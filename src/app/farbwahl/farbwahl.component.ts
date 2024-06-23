df1f51086e31ce4a8741ba0dfc5a0818df1f6def
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-24 00:06:47 +0200

    farbwahl.component.html enthaelt jetzt ion-slider mit Event-Handling

--- src/app/farbwahl/farbwahl.component.ts
+++ src/app/farbwahl/farbwahl.component.ts
@@ -1,14 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-farbwahl',
   templateUrl: './farbwahl.component.html',
   styleUrls: ['./farbwahl.component.scss'],
 })
-export class FarbwahlComponent  implements OnInit {
+export class FarbwahlComponent  {
 
-  constructor() { }
+  /** Wert für Rot-Anteil von Farbe (0..255). */
+  public rotWert : number = 240;
 
-  ngOnInit() {}
+  /** Wert für Grün-Anteil von Farbe (0..255). */
+  public gruenWert : number = 10;
+
+  /** Wert für Balu-Anteil von Farbe (0..255). */
+  public blauWert : number = 10;
+
+  /** Hex-Code der aktuelles ausgewählten Farbe. */
+  public farbeHexCode : string = "";
+
+
+  constructor() { 
+
+    this.onFarbanteilChanged();
+  }
+
+
+  /**
+   * Event-Handler-Methode, die aufgerufen wird, wenn einer der RGB-Farbwerte über
+   * eines der "ion-range"-Elemente geändert wird.
+   */
+  onFarbanteilChanged() {
+
+    const HEX_BASIS_WERT = 16;
+
+    //console.log(`rot=${this.rotWert}, grün=${this.gruenWert}, blau=${this.blauWert}`);
+
+    let rotHex   = this.rotWert.toString(   HEX_BASIS_WERT );
+    let gruenHex = this.gruenWert.toString( HEX_BASIS_WERT );
+    let blauHex  = this.blauWert.toString(  HEX_BASIS_WERT );
+
+    // Jeder Hex-Wert muss zweistellig sein
+    if ( rotHex.length   === 1 ) { rotHex   = "0" + rotHex;   }
+    if ( gruenHex.length === 1 ) { gruenHex = "0" + gruenHex; }
+    if ( blauHex.length  === 1 ) { blauHex  = "0" + blauHex;  }
+
+    this.farbeHexCode = "#" + rotHex + gruenHex + blauHex;
+  }  
 
 }
--- src/app/farbwahl/farbwahl.module.ts
+++ src/app/farbwahl/farbwahl.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { IonicModule } from '@ionic/angular';  // damit auch Ionic-Komponenten verwendet werden können
+import { FormsModule } from '@angular/forms'; // damit auch [(ngModel)] verwendet werden kann
+
 import { FarbwahlComponent } from './farbwahl.component';
 
 
 @NgModule({
-  imports: [ CommonModule ],      
+  imports: [ CommonModule, IonicModule, FormsModule ],      
   declarations: [ FarbwahlComponent ],
   exports: [ FarbwahlComponent ]
 })
 export class FarbwahlModule { }
 
-
-/*
-
-  @NgModule({
-    imports: [ CommonModule, FormsModule, IonicModule],
-    declarations: [ExploreContainerComponent],
-    exports: [ExploreContainerComponent]
-  })
-  export class ExploreContainerComponentModule {}
-
-*/
